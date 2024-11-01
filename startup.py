8cd6448a70838eddf9d5b7ed756b3db0f2b0e4eb
SimpleScript-Language <146978293+Simple-Script@users.noreply.github.com>
2024-11-01 10:15:43 -0500

    Update startup.py

--- startup.py
+++ startup.py
@@ -1,1 +1,10 @@
-hello
+import random
+import time
+import os
+
+keys = ['e', 'r', 'w', 'a', 's', 'd']
+
+while True:
+    key = random.choice(keys)
+    os.system(f"xdotool key {key}")
+    time.sleep(1)
